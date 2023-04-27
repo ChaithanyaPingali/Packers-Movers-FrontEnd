ae500a16b581233252951348a46f1b62d0f12b0b
vamsi2711 <krishnavamsisomu@gmail.com>
2023-04-27 10:18:06 -0500

    updated code for better look

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import { App } from './App'
+import { getAllPackers } from './common/util'
 import reportWebVitals from './reportWebVitals'
 import { Packers } from './components/Packers'
 import { ErrorPage } from './common/ErrorPage'
-import { getAllPackers } from './common/util'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
